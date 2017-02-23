51f2d1972fd31c37f59b322eb09d4840f4871c36
Greg <gjr8050@rit.edu>
2017-02-23 12:46:02 -0500

    use typescript-mocha

--- test/spec/priority-queue.ts
+++ test/spec/priority-queue.ts
@@ -3,8 +3,12 @@
  * Created by gjrwcs on 2/23/2017.
  */
 
+import { only, skip, slow, suite, test, timeout } from "mocha-typescript";
 import {PriorityQueue} from "../../src/priority-queue";
 
-describe('PriorityQueue', () => {
-    const priorityQueue = new PriorityQueue();
-});
+@suite class PriorityQueueSpec {
+
+    @test 'Can instantiate a PriorityQueue'() {
+        const priorityQueue = new PriorityQueue();
+    }
+}
