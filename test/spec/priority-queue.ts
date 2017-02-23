3253fae313fc949fe724b8e0190cc8a81a6df92f
Greg <gjr8050@rit.edu>
2017-02-23 12:25:35 -0500

    add test setup

---
+++ test/spec/priority-queue.ts
@@ -0,0 +1,10 @@
+'use strict';
+/**
+ * Created by gjrwcs on 2/23/2017.
+ */
+
+import {PriorityQueue} from "../../src/priority-queue";
+
+describe('PriorityQueue', () => {
+    const priorityQueue = new PriorityQueue();
+});
