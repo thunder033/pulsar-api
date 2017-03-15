3a8581922c7890e8c6438b3b4e1f338deb1dca04
Greg Rozmarynowycz <gjr8050@rit.edu>
2017-03-14 23:39:16 -0400

    fix cyclic dependency

--- src/network-index.ts
+++ src/network-index.ts
@@ -7,7 +7,6 @@ import * as uuid from 'uuid/v4';
 import {IOEvent} from './event-types';
 import {isNullOrUndefined} from 'util';
 import Socket = SocketIO.Socket;
-import {ServerComponent, SyncServer} from './sync-server';
 import {Component, Composite} from './component';
 
 export interface INetworkEntity {
@@ -28,99 +27,6 @@ export interface INetworkEntity {
  */
 interface INetworkEntityCtor {
     new(...args: any[]): INetworkEntity;
-}
-
-/**
- * Provides arbitrary access to entities that are needed by clients
- */
-export class NetworkIndex extends ServerComponent {
-
-    public types: Map<string, INetworkEntityCtor>;
-    public entities: Map<string, Map<string, INetworkEntity>>;
-
-    constructor(syncServer: SyncServer) {
-        super(syncServer, [Networkable]);
-
-        this.entities = new Map();
-        this.types = new Map();
-
-        Networkable.init(this);
-        NetworkEntity.init(this);
-    }
-
-    /**
-     * Add an entity to the network index
-     * @param type: the type of the entity
-     * @param entity {INetworkEntity}: the entity to add
-     */
-    public putNetworkEntity(type: INetworkEntityCtor, entity: INetworkEntity): void {
-        const keyType: any = this.resolveNetworkEntityType(type);
-
-        if (keyType === null) {
-            throw new TypeError(`Could not resolve ${type} to a valid key type`);
-        }
-
-        this.entities.get(keyType.name).set(entity.getId(), entity);
-    }
-
-    /**
-     * Attempt to resolve a given type to a base type used in the network index
-     * @param type
-     * @returns {any}
-     */
-    public resolveNetworkEntityType(type: INetworkEntityCtor): INetworkEntityCtor {
-        // If the network type is in the index, we're all good
-        if (this.entities.has(type.name)) {
-            return type;
-        } else {
-            // If not, it might be a derived type, so attempt to find a matching base type
-            let resolvedType: any = null;
-            this.types.forEach((candidateType) => {
-                // compare the provided type with with each type in the network index
-                if (type.prototype instanceof candidateType) {
-                    resolvedType = candidateType;
-                }
-            });
-
-            // If a matching base type isn't found, we just return null
-            return resolvedType;
-        }
-    }
-
-    /**
-     * Register an entity type in the network entity index
-     * @param type
-     */
-    public registerType(type: INetworkEntityCtor) {
-        if (!this.types.has(type.name)) {
-            console.log(`Register type ${type.name}`);
-            this.types.set(type.name, type);
-            this.entities.set(type.name, new Map());
-        }
-    }
-
-    /**
-     * Get the type associated with a given type name
-     * @param name
-     * @returns {undefined|INetworkEntityCtor}
-     */
-    public getType(name: string): INetworkEntityCtor {
-        return this.types.get(name);
-    }
-
-    public getById<T extends INetworkEntity>(type: INetworkEntityCtor, id: string): T {
-        const keyType: any = this.resolveNetworkEntityType(type);
-
-        if (keyType === null) {
-            throw new TypeError(`Could not resolve ${type} to a valid key type`);
-        }
-
-        return this.entities.get(keyType.name).get(id) as T;
-    }
-
-    public sync(entity: INetworkEntity, roomName: string): void {
-        this.server.broadcast(IOEvent.syncNetworkEntity, new SyncResponse(entity), roomName);
-    }
 }
 
 /**
@@ -247,5 +153,100 @@ export class Networkable extends Component implements INetworkEntity {
         } else {
             Networkable.networkIndex.sync(this, roomName);
         }
+    }
+}
+
+import {ServerComponent, SyncServer} from './sync-server';
+
+/**
+ * Provides arbitrary access to entities that are needed by clients
+ */
+export class NetworkIndex extends ServerComponent {
+
+    public types: Map<string, INetworkEntityCtor>;
+    public entities: Map<string, Map<string, INetworkEntity>>;
+
+    constructor(syncServer: SyncServer) {
+        super(syncServer, [Networkable]);
+
+        this.entities = new Map();
+        this.types = new Map();
+
+        Networkable.init(this);
+        NetworkEntity.init(this);
+    }
+
+    /**
+     * Add an entity to the network index
+     * @param type: the type of the entity
+     * @param entity {INetworkEntity}: the entity to add
+     */
+    public putNetworkEntity(type: INetworkEntityCtor, entity: INetworkEntity): void {
+        const keyType: any = this.resolveNetworkEntityType(type);
+
+        if (keyType === null) {
+            throw new TypeError(`Could not resolve ${type} to a valid key type`);
+        }
+
+        this.entities.get(keyType.name).set(entity.getId(), entity);
+    }
+
+    /**
+     * Attempt to resolve a given type to a base type used in the network index
+     * @param type
+     * @returns {any}
+     */
+    public resolveNetworkEntityType(type: INetworkEntityCtor): INetworkEntityCtor {
+        // If the network type is in the index, we're all good
+        if (this.entities.has(type.name)) {
+            return type;
+        } else {
+            // If not, it might be a derived type, so attempt to find a matching base type
+            let resolvedType: any = null;
+            this.types.forEach((candidateType) => {
+                // compare the provided type with with each type in the network index
+                if (type.prototype instanceof candidateType) {
+                    resolvedType = candidateType;
+                }
+            });
+
+            // If a matching base type isn't found, we just return null
+            return resolvedType;
+        }
+    }
+
+    /**
+     * Register an entity type in the network entity index
+     * @param type
+     */
+    public registerType(type: INetworkEntityCtor) {
+        if (!this.types.has(type.name)) {
+            console.log(`Register type ${type.name}`);
+            this.types.set(type.name, type);
+            this.entities.set(type.name, new Map());
+        }
+    }
+
+    /**
+     * Get the type associated with a given type name
+     * @param name
+     * @returns {undefined|INetworkEntityCtor}
+     */
+    public getType(name: string): INetworkEntityCtor {
+        return this.types.get(name);
+    }
+
+    public getById<T extends INetworkEntity>(type: INetworkEntityCtor, id: string): T {
+        const keyType: any = this.resolveNetworkEntityType(type);
+
+        if (keyType === null) {
+            throw new TypeError(`Could not resolve ${type} to a valid key type`);
+        }
+
+        return this.entities.get(keyType.name).get(id) as T;
+    }
+
+    public sync(entity: INetworkEntity, roomName: string): void {
+        this.server.broadcast(IOEvent.syncNetworkEntity, new SyncResponse(entity), roomName);
     }
 }
