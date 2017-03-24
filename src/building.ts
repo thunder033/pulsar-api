9dbf4a05eab62ee03bc7d6445c28cfe15747b47c
Greg Rozmarynowycz <gjr8050@rit.edu>
2017-03-23 23:49:05 -0400

    add server request for client rooms

--- src/building.ts
+++ src/building.ts
@@ -1,10 +1,25 @@
 import {ServerComponent, SyncServer} from './sync-server';
 import {Room} from './room';
-import {Client} from './client';
+import {Client, ClientComponent} from './client';
 import {IOEvent} from 'event-types';
 /**
  * Created by gjrwcs on 3/6/2017.
  */
+
+export class Occupant extends ClientComponent {
+    public onInit() {
+        this.socket.on('requestRooms', this.getRooms.bind(this));
+    }
+
+    private getRooms(data) {
+        const req = data.body;
+        // const errorKey = `${IOEvent.serverError}-${data.reqId}`;
+        console.log(`${data.reqId}: ${req.type} ${req.id}`);
+
+        const rooms = this.server.getComponent(Building).getClientRooms(this.user);
+        this.socket.emit(`${'requestRooms'}-${data.reqId}`, rooms.map((room) => room.getId()));
+    }
+}
 
 export class Building extends ServerComponent {
 
@@ -12,7 +27,7 @@ export class Building extends ServerComponent {
     private defaultRoom: Room;
 
     constructor(syncServer: SyncServer) {
-        super(syncServer);
+        super(syncServer, [Occupant]);
 
         this.rooms = new Map<string, Room>();
         this.defaultRoom = null;
@@ -59,6 +74,21 @@ export class Building extends ServerComponent {
         this.rooms.forEach((room) => {
             room.remove(user);
         });
+    }
+
+    /**
+     * Retrive the the list of rooms a given client is in
+     * @param user
+     * @returns {Array}
+     */
+    public getClientRooms(user: Client): Room[] {
+        const clientRooms: Room[] = [];
+        this.rooms.forEach((room) => {
+            if (room.contains(user)) {
+                clientRooms.push(room);
+            }
+        });
+        return clientRooms;
     }
 
     public syncClient(socket: SocketIO.Socket): void {
