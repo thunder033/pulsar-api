45f19ab7fb77316f04d7e5e852ed7a887e179c6d
Greg Rozmarynowycz <crashtest1@live.com>
2017-03-24 04:42:23 -0400

    include other tsconfig

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,6 +58,7 @@ module.exports = (grunt) => {
                     'public/views/**',
 
                     'src/**/*.js',
+                    'src/tsconfig.json',
                     // angular js included in dist bundle
                     '!src/ng/**/*.js',
 
