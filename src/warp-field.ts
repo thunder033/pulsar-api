592182a39ae4b4334c31a301dc274ccd99f8c838
Greg Rozmarynowycz <greg@thunderlab.net>
2017-04-03 15:52:07 -0400

    add speed to warp slice

--- src/warp-field.ts
+++ src/warp-field.ts
@@ -6,10 +6,12 @@ import {NetworkEntity} from './network-index';
 export class LevelSlice {
     private gems: number[];
     private loudness: number;
+    private speed: number;
 
-    constructor(gems: number[]) {
-        this.loudness = 1;
+    constructor(gems: number[], loudness: number, speed: number) {
+        this.loudness = loudness;
         this.gems = gems;
+        this.speed = speed;
     }
 }
 
@@ -26,15 +28,20 @@ export class WarpField extends NetworkEntity {
         const lanes = 3;
         const length = 250;
 
+        const DEG_TO_RAD = Math.PI / 180;
         for (let i = 0; i < length; i++) {
             const gems = new Array(lanes);
             gems.fill(0);
             gems[i % lanes] = 1;
-            this.level[i] = new LevelSlice(gems);
+            this.level[i] = new LevelSlice(gems, Math.sin(i * DEG_TO_RAD), Math.cos(i * DEG_TO_RAD));
         }
 
         this.timeStep = 500; // ms
         this.duration = length * this.timeStep;
+    }
+
+    public reconstruct(buffer: Buffer) {
+        throw new Error('not implemented');
     }
 
     public getTimeStep(): number {
