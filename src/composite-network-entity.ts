666c4d0fb4b657500f9c3a00ad23135ecb7f55a1
Greg Rozmarynowycz <crashtest1@live.com>
2017-04-06 08:24:05 -0400

    create composite network entity, add component iface

---
+++ src/composite-network-entity.ts
@@ -0,0 +1,30 @@
+/**
+ * Created by Greg on 4/5/2017.
+ */
+import {Composite, IComponentCtor} from './component';
+import {INetworkEntity, Networkable} from './network-index';
+
+export class CompositeNetworkEntity extends Composite implements INetworkEntity {
+    constructor(componentTypes: IComponentCtor[] = []) {
+        componentTypes.unshift(Networkable);
+        super(componentTypes);
+    }
+
+    public getSerializable(): Object {
+        return {
+            id: this.getId(),
+        };
+    }
+
+    public getId(): string {
+        return this.getComponent(Networkable).getId();
+    }
+
+    public getType() {
+        return this.getComponent(Networkable).getType();
+    }
+
+    public sync(socket?: SocketIO.Socket): void {
+        this.getComponent(Networkable).sync(socket);
+    }
+}
